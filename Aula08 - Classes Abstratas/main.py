8ee65fa47e126962577912dc341eb3134a5a979f
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2025-04-23 10:29:13 -0300

    Aula08 - Classes Abstratas

---
+++ Aula08 - Classes Abstratas/main.py
@@ -0,0 +1,10 @@
+#from Cliente import Cliente
+#c = Cliente("João")
+
+from Fisico import Fisico
+#f = Fisico("João", "000.111.222-33")
+f = Fisico(None)
+f.cadastrar()
+f.limite = 1000
+f.limite = 500
+f.imprimir()
